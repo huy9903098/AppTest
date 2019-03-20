f992a95811b20ad91ae0445c2c1a4fb96b5b2536
huy <skyzerzlol@gmail.com>
2019-03-20 12:51:17 +0200

    test

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const validation = require('validation');
 const app = express();
 
-app.get('/',(res,req)=> res.send('Hello') );
+app.get('/',(res,req)=> res.send('Goodbye') );
+
+app.get('/god',(res,req)=> res.send('God Hello') );
