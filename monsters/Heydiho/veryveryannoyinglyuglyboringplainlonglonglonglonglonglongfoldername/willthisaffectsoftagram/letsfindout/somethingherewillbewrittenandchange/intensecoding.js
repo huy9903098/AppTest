2fed06a88f0e0a3c8c36d4bbca8538cdc6cc687f
Unknown <skyzerzlol@gmail.com>
2019-03-21 08:49:47 +0200

    dam

--- monsters/Heydiho/veryveryannoyinglyuglyboringplainlonglonglonglonglonglongfoldername/willthisaffectsoftagram/letsfindout/somethingherewillbewrittenandchange/intensecoding.js
+++
@@ -1,6 +0,0 @@
-const coding = false;
-
-if (coding == false) {
-    coding = !true;
-    console.log("You work hard");
-}
---
+++ monsters/Heydiho/veryveryannoyinglyuglyboringplainlonglonglonglonglonglongfoldername/willthisaffectsoftagram/letsfindout/somethingherewillbewrittenandchange/jame.js
@@ -0,0 +1,7 @@
+const jame = "boi";
+
+if (jame == "boi") {
+    console.log("failure");
+} else {
+    console.log("still failure");
+}
